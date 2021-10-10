c0f8530a02310314923fd6bb528c843d8cca1bd5
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-10-10 16:36:07 -0300

    understanding useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { Item } from "./components/Item";
 
 function App() {
   const [items, setItems] = useState<string[]>([]);
-  const [newItem, setNewItem] = useState('')
+  const [wishlist, setWishlist] = useState<string[]>([]);
+  const [newItem, setNewItem] = useState("");
 
   function addItemToList() {
-    setItems([...items, `Item ${items.length}`])
+    setItems([...items, `Item ${items.length}`]);
   }
 
-  // contagem se repete a cada renderização do componente
-  // const countItemsWithOne = items.filter(item => item.includes('1')).length
-  // console.log('item calculado', countItemsWithOne)
+  const addItemToWishlist = useCallback((item: string) => {
+    setWishlist((state) => [...state, item]);
+  }, []);
 
-  const countItemsWithOne = useMemo(()=> {
-    console.log('item calculado') // só executa quando o valor de items muda
-    return items.filter(item => item.includes('1')).length
-  },[items])
+  const countItemsWithOne = useMemo(() => {
+    console.log("item calculado"); // só executa quando o valor de items muda
+    return items.filter((item) => item.includes("1")).length;
+  }, [items]);
 
   return (
     <div>
-      <input type="text" onChange={e => setNewItem(e.target.value)} value={newItem}/>
+      <input
+        type="text"
+        onChange={(e) => setNewItem(e.target.value)}
+        value={newItem}
+      />
       <button onClick={addItemToList}>Add</button>
       <ul>
-        {items.map(item => {
-          return <Item key={item} title={item} />
+        {items.map((item) => {
+          return (
+            <Item key={item} onAddToWishlist={addItemToWishlist} title={item} />
+          );
         })}
       </ul>
-      <div>
-        Contagem: {countItemsWithOne}
-      </div>
+      <div>Contagem: {countItemsWithOne}</div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,10 +2,18 @@ import { memo } from "react";
 
 type ItemProps = {
   title: string;
+  onAddToWishlist: (item: string) => void;
 };
 
 function ItemComponent(props: ItemProps) {
-  return <li>{props.title}</li>;
+  return (
+    <li>
+      {props.title}{" "}
+      <button onClick={() => props.onAddToWishlist(props.title)}>
+        Add to wishlist
+      </button>
+    </li>
+  );
 }
 
 export const Item = memo(ItemComponent);
